09d81eb1f2107add26a91c25abd130d455cc7742
madhumathi <madhumathi2201@gmail.com>
2025-05-04 09:20:16 +0530

    db changes

--- Database/db.config.js
+++ Database/db.config.js
@@ -3,11 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectionDB = async () => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB connected: ${connect.connection.host}`);
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${connect.connection.host}`);
         
     } catch (error) {
         console.log(`Error: ${error.message}`);
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import router from './Routers/CrudRoute.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.port
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
